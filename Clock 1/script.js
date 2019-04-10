aa10a5a5e12d7614927c030bb544d8db74738349
LutherCrowther <45578229+LutherCrowther@users.noreply.github.com>
2019-04-10 19:03:46 +1200

    Added manual arm movements

--- Clock 1/script.js
+++ Clock 1/script.js
@@ -1,3 +1,13 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+//Create vairable for calculating arm postions
+let hrPosition = 20;
+let minPosition = 130;
+let secPosition = 267;
+//Math part for degree conversion
+
+HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
